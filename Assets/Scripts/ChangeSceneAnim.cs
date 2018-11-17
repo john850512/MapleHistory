1eee860bd0216c9722cdd947f9b14f54e67497bb
john850512 <john85051232@gmail.com>
2018-11-18 02:58:15 +0800

    upload code&files

---
+++ Assets/Scripts/ChangeSceneAnim.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeSceneAnim : StateMachineBehaviour {
+    //state紀錄切換的狀態
+    //0: 從Main到Choose
+    //1: 從Choose到Main
+    public int state;
+
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        if (this.state == 0) { //開始動畫到進入主畫面
+            SceneManager.LoadScene("MainScene");
+        }
+        else if (this.state == 1) { //主畫面到選單
+            SceneManager.LoadScene("ChooseChapterScene");
+        }
+        else if (this.state == 2)
+        { //選單回主畫面
+            SceneManager.LoadScene("MainScene");
+            //animator.GetComponent<LoadingScript>().DoCoroutine();
+        }
+
+        
+        
+	}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+}
---
+++ Assets/Scripts/ChooseChapterScene_Controller.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChooseChapterScene_Controller : MonoBehaviour {
+    public Animator ChapterList;
+    public AudioSource audio_RollDown;
+    public AudioSource audio_RollUp;
+    public AudioSource audio_Click;
+    public AudioSource audio_GameIn;
+    public Text ChapterName;
+    private string[] ChapterName_List = { "相遇", 
+                                          "沒有終點的陪伴",
+                                          "騎士，有沒有用？",
+                                          "１００％的使命",
+                                          "讓我分擔",
+                                          "歸宿",
+                                          "妳不在",
+                                          "難道",
+                                          "沒有發現，妳的難過",
+                                          "轉折",
+                                          "妳的溫柔",
+                                          "原來",
+                                          "To Marry",
+                                          "齁！齁！",
+                                          "太美麗",
+                                          "這一年",
+                                          "好想見妳",
+                                          "明日過後",
+                                          "再見了",
+                                          "是妳，改變了我，在這MapleStory" };
+    // 點擊聲音特效
+    private bool Anim_flag;
+    private int Anim_CH;
+
+    public Button btn_GameIn;
+    private int Chapter_Num;
+
+    // background music-detect
+    public GameObject BackgroundMusic;
+    private GameObject audioPlayer; // holds a reference to the audio player
+    public string BackgroundMusic_name = "BackgroundMusic"; //用字串紀錄vackground music的prefeb名稱，到時要改比較方便
+
+    
+
+	// Use this for initialization
+	void Start () {
+        ChapterName.text = "";
+        Anim_CH = -1;
+        Chapter_Num = -1;
+        btn_GameIn.gameObject.SetActive(false);
+
+        // background music-detect
+        audioPlayer = GameObject.Find(BackgroundMusic_name+"(Clone)");
+        if (audioPlayer == null) //if DontDestroyOnLoad Object doesnt exist
+        {
+            audioPlayer = Instantiate(BackgroundMusic, transform.position, transform.rotation) as GameObject;
+            audioPlayer.GetComponent<AudioSource>().Play();
+            MainScene_Controller.music_is_play_flag = true;
+            DontDestroyOnLoad(audioPlayer); 
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    private void check_anim_parameters(bool Anim_flag, int Anim_CH, int CH)
+    {
+        //Debug.Log(CH);
+        if (Anim_CH == -1)//第一次的時候要執行張開動畫
+        {
+            Chapter_Num = CH;
+            btn_GameIn.gameObject.SetActive(true);
+            ChapterList.SetInteger("CH", CH);
+            ChapterList.SetBool("Play", true);
+            audio_RollDown.Play();
+            ChapterName.text = ChapterName_List[CH];
+        }
+        else if (Anim_CH == CH) //相同章節的張開縮和判斷
+        {
+            if (Anim_flag == false)
+            {
+                Chapter_Num = CH;
+                btn_GameIn.gameObject.SetActive(true);
+                ChapterList.SetBool("Play", true);
+                audio_RollDown.Play();
+                ChapterName.text = ChapterName_List[CH];
+            }
+            else
+            {
+                ChapterName.text = "";
+                btn_GameIn.gameObject.SetActive(false);
+                ChapterList.SetBool("Play", false);
+                ChapterList.SetInteger("CH", -1);
+                audio_RollUp.Play();
+            }
+        }
+        else //轉換到不同章節的時候也要馬上執行張開動畫
+        {
+            
+            btn_GameIn.gameObject.SetActive(true);
+            Chapter_Num = CH;
+            ChapterList.SetInteger("CH", CH);
+            audio_RollDown.Play();
+            ChapterName.text = ChapterName_List[CH];
+        }
+    }
+
+    public void ClickChapterListBtn(int CH) {
+        Anim_flag = ChapterList.GetBool("Play");
+        Anim_CH = ChapterList.GetInteger("CH");
+        check_anim_parameters(Anim_flag, Anim_CH, CH);
+    }
+
+    public void ReturnMainScene()
+    {
+        audio_Click.Play();
+        StartCoroutine("DelayFunction",0.1);
+        
+    }
+    public void ChangeChapterScene()
+    {
+        audio_Click.Play();
+        audio_GameIn.Play();
+        StartCoroutine("Load_Chpater");
+
+
+    }
+    IEnumerator DelayFunction(float Count)
+    {
+        yield return new WaitForSeconds(0.1f); //Count is the amount of time in seconds that you want to wait.
+        //And here goes your method of resetting the game...
+        SceneManager.LoadScene("Anim_From_Choose_To_Main");
+        yield return null;
+    }
+
+    IEnumerator Load_Chpater()
+    {
+        yield return new WaitForSeconds(0.1f); //Count is the amount of time in seconds that you want to wait.
+        //And here goes your method of resetting the game...
+        Destroy(audioPlayer);
+        SceneManager.LoadScene("Chapter" + Chapter_Num);
+        yield return null;
+    }
+}
